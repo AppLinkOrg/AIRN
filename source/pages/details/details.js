c46cc20ea8c8c78bafa97a17670b9db14349671e
Tyrant24 <tigerhi@qq.com>
2020-02-10 22:59:53 +0800

    123

--- source/pages/details/details.js
+++ source/pages/details/details.js
@@ -11,12 +11,20 @@ class Content extends AppBase {
     this.Base.Page = this;
     //options.id=5;
     super.onLoad(options);
+    console.log(options.types,'类型')
     
     if (options.tps != undefined && options.tps != null){
-      this.Base.setMyData({ tps: options.tps, barcode: options.barcode})
-    }else{
+      this.Base.setMyData({ tps: options.tps, barcode: options.barcode, jsondatalist: options.barcode})
+    }
+    else if (options.types=="A"){
+      console.log('走不走')
       var jsondata = JSON.parse(options.json);
-      this.Base.setMyData({ jsondata });
+      this.Base.setMyData({ jsondata, types: options.types});
+    }
+    else{
+      console.log('看看')
+      var jsondata = JSON.parse(options.json);
+      this.Base.setMyData({ jsondata, jsondatalist: jsondata.item_list });
     }
    // this.Base.setMyData({ show:0})
   }
@@ -33,10 +41,40 @@ class Content extends AppBase {
     })
 
   }
+  allcopy(e){
+    
+    var jsondata = this.Base.getMyData().jsondatalist; 
+    var jsondata2 = this.Base.getMyData().jsondata;
+    var list = '';
+
+    if (this.Base.options.tps != undefined && this.Base.options.tps != null) {
+      wx.setClipboardData({
+        data: jsondata,
+      })
+    }
+    else if (this.Base.getMyData().types=='A'){
+      list = '姓名：' + jsondata2.name + '\n' + '性别：' + jsondata2.sex + '\n' + '民族：' + jsondata2.nation + '\n' + '出生：' + jsondata2.birth + '\n' + '住址：' + jsondata2.address + '\n' + '公民身份号码：' + jsondata2.id
+
+      wx.setClipboardData({
+        data: list,
+      })
+    }
+    else{
+      for (var i = 0; i < jsondata.length; i++) {
+        list += jsondata[i].item + ':' + jsondata[i].itemstring + '\n';
+      }
+      wx.setClipboardData({
+        data: list,
+      })
+
+    }
+     
+  }
 }
 var content = new Content();
 var body = content.generateBodyJson();
 body.onLoad = content.onLoad;
 body.onMyShow = content.onMyShow; 
-body.copy = content.copy;
+body.copy = content.copy; 
+body.allcopy = content.allcopy; 
 Page(body)
